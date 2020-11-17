d6952ba97eb9431597a66569f20cb9892620930c
DrEremin <ivaneremin1979@gmail.com>
2020-11-17 20:04:06 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args)  {
+
+        final String SITE = "https://www.sql.ru/forum/job-offers";
+        String[] str = new String[]{"разработчик"};
+        Page page = new Page(SITE, str, 3);
+
+        page.parsePages();
+        VacancyToFile out = new VacancyToFile();
+        out.output();////////////
+    }
+}
+    /*final String SITE = "https://www.sql.ru/forum/job-offers"; separatorChar*/
